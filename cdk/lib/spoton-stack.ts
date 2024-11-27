09b22700e47e4b0f1a0a5769e7ddbbfbf249f7e0
namse <skatpgusskat@naver.com>
2024-11-27 04:47:42 +0000

    Set up cdk

--- cdk/lib/spoton-stack.ts
+++ cdk/lib/spoton-stack.ts
@@ -1,16 +1,70 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from "path";
 
 export class SpotonStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const janitorLambda = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "JanitorLambda",
+      {
+        entry: path.join(__dirname, "../../janitor/index.js"),
+        initialPolicy: [
+          new cdk.aws_iam.PolicyStatement({
+            actions: [
+              "ec2:CreateSnapshot",
+              "ec2:DeleteSnapshot",
+              "ec2:DeleteVolume",
+              "ec2:DescribeSnapshots",
+              "ec2:DescribeVolumes",
+            ],
+            resources: ["*"],
+          }),
+        ],
+        architecture: cdk.aws_lambda.Architecture.ARM_64,
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SpotonQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const janitorEventsRule = new cdk.aws_events.Rule(
+      this,
+      "JanitorEventsRule",
+      {
+        schedule: cdk.aws_events.Schedule.rate(cdk.Duration.minutes(5)),
+      }
+    );
+
+    janitorEventsRule.addTarget(
+      new cdk.aws_events_targets.LambdaFunction(janitorLambda)
+    );
+
+    const starterLambda = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "StarterLambda",
+      {
+        entry: path.join(__dirname, "../../starter/index.js"),
+        initialPolicy: [
+          new cdk.aws_iam.PolicyStatement({
+            actions: [
+              "ec2:DescribeInstances",
+              "ec2:DescribeSnapshots",
+              "ec2:DescribeSpotPriceHistory",
+              "ec2:RunInstances",
+            ],
+            resources: ["*"],
+          }),
+        ],
+        architecture: cdk.aws_lambda.Architecture.ARM_64,
+      }
+    );
+
+    const starterLambdaFunctionUrl = starterLambda.addFunctionUrl({
+      authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "StarterLambdaFunctionUrl", {
+      value: starterLambdaFunctionUrl.url,
+    });
   }
 }
